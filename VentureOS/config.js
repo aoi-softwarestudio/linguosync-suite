dcc2348575ecdcfa273975bddf34e8b8e2bff4fb
kojimasota3-art <kojimasota3@gmail.com>
2026-05-22 11:14:21 +0900

    Deploy update: VentureOS/config.js

---
+++ VentureOS/config.js
@@ -0,0 +1,6 @@
+const VENTURE_LINKS = {
+    studyflow: "https://skiing-mesh-threatened-final.trycloudflare.com",
+    novacapital: "https://featured-limit-transition-neighborhood.trycloudflare.com",
+    linguosync: "https://coastal-provision-hearings-installing.trycloudflare.com",
+    socialintent: "https://breath-constraints-style-dvds.trycloudflare.com"
+};
